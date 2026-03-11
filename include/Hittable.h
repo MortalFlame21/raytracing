7ad0a984c2cb7cd054225e62b276c434c87a7a3f
MortalFlame21 <markgio2100@gmail.com>
2026-03-11 11:42:33 +1100

    Hittable.h. HittableList.h removed. Moved to Hittable.h.

--- include/Hittable.h
+++ include/Hittable.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "Ray.h"
 
 class HitRecord {
@@ -29,3 +32,18 @@ private:
     Vec3 m_center;
     double m_radius;
 };
+
+class HittableList : Hittable {
+public:
+    HittableList();
+    HittableList(std::vector<std::shared_ptr<Hittable>> objects);
+
+    void clear();
+    void push_back(std::shared_ptr<Hittable> object);
+    bool hit(const Ray& r, double ray_t_min, double ray_t_max,
+             HitRecord& rec) const override;
+
+private:
+    std::vector<std::shared_ptr<Hittable>> m_objects;
+};
+;
--- src/Hittable.cpp
+++ src/Hittable.cpp
@@ -2,12 +2,15 @@
 
 #include "Hittable.h"
 
+// Start HitRecord
 void HitRecord::set_face_normal(const Ray& r, const Vec3& outward_normal) {
     const auto front_face{dot(r.direction(), outward_normal) < 0};
     // either ray is inside the sphere or outside.
     m_normal = front_face ? outward_normal : -outward_normal;
 }
+// End HitRecord
 
+// Start Sphere
 Sphere::Sphere() : m_center{}, m_radius{} {};
 
 Sphere::Sphere(const Vec3& center, double radius)
@@ -39,3 +42,34 @@ bool Sphere::hit(const Ray& r, double ray_t_min, double ray_t_max, HitRecord& re
 
     return true;
 }
+// End Sphere
+
+// Start HittableList
+HittableList::HittableList() : m_objects{} {}
+
+HittableList::HittableList(std::vector<std::shared_ptr<Hittable>> objects)
+    : m_objects{objects} {}
+
+void HittableList::clear() { m_objects.clear(); }
+
+void HittableList::push_back(std::shared_ptr<Hittable> object) {
+    m_objects.push_back(object);
+}
+
+bool HittableList::hit(const Ray& r, double ray_t_min, double ray_t_max,
+                       HitRecord& rec) const {
+    HitRecord temp_rec{};
+    bool hit_anything{};
+    auto closest_so_far{ray_t_max};
+
+    for (const auto& o : m_objects) {
+        if (o->hit(r, ray_t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.m_time;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
+// End HittableList
