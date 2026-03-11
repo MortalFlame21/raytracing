2d00cd168d5ee9dabbe1975b2cb66285d125717f
MortalFlame21 <markgio2100@gmail.com>
2026-03-12 10:34:13 +1100

    Camera.h. Add anti-aliasing.

--- include/Camera.h
+++ include/Camera.h
@@ -6,11 +6,13 @@
 class Camera {
 public:
     Camera();
-    Camera(double aspect_ratio, int img_h);
+    Camera(double aspect_ratio, int img_h, int samples_per_pixel);
     void render(const Hittable& world);
 
 private:
     double m_aspect_ratio;
+    double m_pixel_samples_scale;
+    int m_samples_per_pixel;
     int m_img_w;
     int m_img_h;
     Vec3 m_center;
@@ -20,4 +22,6 @@ private:
 
     void initialise();
     Color ray_color(const Ray& r, const Hittable& world) const;
+    Ray get_ray(int i, int j) const;
+    Vec3 sample_square() const;
 };
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -3,10 +3,11 @@
 #include "Interval.h"
 #include "Util.h"
 
-Camera::Camera() : Camera(1.0, 100) {}
+Camera::Camera() : Camera(1.0, 100, 10) {}
 
-Camera::Camera(double aspect_ratio, int img_w)
-    : m_aspect_ratio{aspect_ratio}, m_img_w{img_w}, m_img_h{}, m_center{},
+Camera::Camera(double aspect_ratio, int img_w, int samples_per_pixel)
+    : m_aspect_ratio{aspect_ratio}, m_pixel_samples_scale{},
+      m_samples_per_pixel{samples_per_pixel}, m_img_w{img_w}, m_img_h{}, m_center{},
       m_pixel00_loc{}, m_pixel_delta_u{}, m_pixel_delta_v{} {
     initialise();
 }
@@ -17,13 +18,19 @@ void Camera::render(const Hittable& world) {
     for (int j{}; j < m_img_h; ++j) {
         std::clog << "\rScanlines remaining: " << (m_img_h - j) << ' ' << std::flush;
         for (int i{}; i < m_img_w; ++i) {
-            auto pixel_center{m_pixel00_loc + (i * m_pixel_delta_u) +
-                              (j * m_pixel_delta_v)};
-            auto ray_direction{pixel_center - m_center};
-            Ray r(m_center, ray_direction);
-            auto pixel_color{ray_color(r, world)};
+            // auto pixel_center{m_pixel00_loc + (i * m_pixel_delta_u) +
+            //                   (j * m_pixel_delta_v)};
+            // auto ray_direction{pixel_center - m_center};
+            // Ray r(m_center, ray_direction);
+            // auto pixel_color{ray_color(r, world)};
 
-            write_color(pixel_color);
+            Color pixel_color{};
+            for (int sample{}; sample < m_samples_per_pixel; ++sample) {
+                Ray r { get_ray(i, j) };
+                pixel_color += ray_color(r, world);
+            }
+
+            write_color(m_pixel_samples_scale * pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";
@@ -32,6 +39,8 @@ void Camera::render(const Hittable& world) {
 void Camera::initialise() {
     m_img_h = static_cast<int>(m_img_w / m_aspect_ratio);
     m_img_h = (m_img_h < 1) ? 1 : m_img_h;
+
+    m_pixel_samples_scale = 1.0 / m_samples_per_pixel;
 
     auto focal_length{1.0};
     auto viewport_h{2.0};
@@ -56,4 +65,17 @@ Color Camera::ray_color(const Ray& r, const Hittable& world) const {
     auto unit_direction{unit_vector(r.direction())};
     auto a{0.5 * (unit_direction.y() + 1.0)};
     return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
+}
+
+Ray Camera::get_ray(int i, int j) const {
+    auto offset{sample_square()};
+    auto pixel_sample{m_pixel00_loc
+                    + ((i + offset.x()) * m_pixel_delta_u)
+                    + ((j + offset.y()) * m_pixel_delta_v)};
+    auto ray_direction{pixel_sample - m_center};
+    return Ray(m_center, ray_direction);
+}
+
+Vec3 Camera::sample_square() const {
+    return Vec3(gen_random_double() - 0.5, gen_random_double() - 0.5, 0);
 }
