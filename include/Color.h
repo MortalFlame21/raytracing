c377f3e04cfdc210df345634e6d695a18ef73f9c
MortalFlame21 <markgio2100@gmail.com>
2026-03-11 23:28:25 +1100

    Color.h. write_color update using clamp.

--- include/Color.h
+++ include/Color.h
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "Vec3.h"
+#include "Interval.h"
 
 using Color = Vec3;
 
@@ -11,9 +12,10 @@ inline void write_color(const Color& pixel_color) {
     auto g{pixel_color.y()};
     auto b{pixel_color.z()};
 
-    auto rbyte{static_cast<int>(255.999 * r)};
-    auto gbyte{static_cast<int>(255.999 * g)};
-    auto bbyte{static_cast<int>(255.999 * b)};
+    static const Interval intensity(0.000, 0.999);
+    auto rbyte{256 * intensity.clamp(r)};
+    auto gbyte{256 * intensity.clamp(g)};
+    auto bbyte{256 * intensity.clamp(b)};
 
     std::cout << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
