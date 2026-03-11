01304781b3aade16f0d81c74d4d57c6b644a4f28
MortalFlame21 <markgio2100@gmail.com>
2026-03-12 10:34:26 +1100

    main.cpp Update main.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 constexpr auto g_ASPECT_RATIO{16.0 / 9.0};
 constexpr auto g_WIDTH{400};
+constexpr auto g_SAMPLES_PER_PIXEL{100};
 
 int main() {
     // world
@@ -14,6 +15,6 @@ int main() {
                         std::make_shared<Sphere>(Vec3(0, -100.5, -1), 100)});
 
     // camera
-    Camera camera(g_ASPECT_RATIO, g_WIDTH);
+    Camera camera(g_ASPECT_RATIO, g_WIDTH, g_SAMPLES_PER_PIXEL);
     camera.render(world);
 }
