82d8ac2a4a3ed88cb0ff22c0a3288c055efadc18
MortalFlame21 <markgio2100@gmail.com>
2026-03-11 11:53:04 +1100

    main.cpp. Update. New background.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 #include "Color.h"
+#include "Hittable.h"
 #include "Ray.h"
+#include "Util.h"
 #include "Vec3.h"
 
 double hit_sphere(const Vec3& center, double radius, const Ray& r) {
@@ -16,12 +18,10 @@ double hit_sphere(const Vec3& center, double radius, const Ray& r) {
     return (discriminant < 0) ? -1.0 : (h - std::sqrt(discriminant)) / a;
 }
 
-Color ray_color(const Ray& r) {
-    auto t { hit_sphere(Vec3(0, 0, -1), 0.5, r) };
-    if (t > 0.0) {
-        auto n { unit_vector(r.at(t) - Vec3(0, 0, -1))};
-        return 0.5 * Color(n.x() + 1, n.y() + 1, n.z() + 1);
-    }
+Color ray_color(const Ray& r, const Hittable& world) {
+    HitRecord rec{};
+    if (world.hit(r, 0, infinity, rec))
+        return 0.5 * (rec.m_normal + Color(1, 1, 1));
 
     auto unit_direction{unit_vector(r.direction())};
     auto a{0.5 * (unit_direction.y() + 1.0)};
@@ -34,6 +34,12 @@ int main() {
     auto img_h{static_cast<int>(img_w / aspect_ratio)};
     img_h = (img_h < 1) ? 1 : img_h;
 
+    // world
+    HittableList world{};
+    world.push_back(std::make_shared<Sphere>(Vec3(0, 0, -1), 0.5));
+    world.push_back(std::make_shared<Sphere>(Vec3(0, -100.5, -1), 100));
+
+    // camera
     auto focal_length{1.0};
     auto viewport_h{2.0};
     auto viewport_w{viewport_h * (static_cast<double>(img_w) / img_h)};
@@ -50,7 +56,7 @@ int main() {
     auto pixel00_loc{viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)};
 
     // header
-    std::cout << "P3\n" << img_w << ' ' << img_w << "\n255\n";
+    std::cout << "P3\n" << img_w << ' ' << img_h << "\n255\n";
 
     for (int j{}; j < img_h; ++j) {
         std::clog << "\rScanlines remaining: " << (img_h - j) << ' ' << std::flush;
@@ -58,7 +64,7 @@ int main() {
             auto pixel_center{pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v)};
             auto ray_direction{pixel_center - camera_center};
             Ray r(camera_center, ray_direction);
-            auto pixel_color{ray_color(r)};
+            auto pixel_color{ray_color(r, world)};
 
             write_color(pixel_color);
         }
