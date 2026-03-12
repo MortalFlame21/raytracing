8d878e07a44a586f6d50ccc11facaf835d1a4b41
MortalFlame21 <markgio2100@gmail.com>
2026-03-12 11:35:26 +1100

    Color.h. Make linear_to_gamma. Update write_color.

--- include/Color.h
+++ include/Color.h
@@ -7,10 +7,18 @@
 
 using Color = Vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    return (linear_component > 0.0) ? std::sqrt(linear_component) : 0.0;
+}
+
 inline void write_color(const Color& pixel_color) {
     auto r{pixel_color.x()};
     auto g{pixel_color.y()};
     auto b{pixel_color.z()};
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const Interval intensity(0.000, 0.999);
     auto rbyte{256 * intensity.clamp(r)};
