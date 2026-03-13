fe1b1ffc5a94d7fa6db0f1e9489b737df9b4da11
MortalFlame21 <markgio2100@gmail.com>
2026-03-13 19:04:50 +1100

    Vec3.cpp. Fix operator*.

--- src/Vec3.cpp
+++ src/Vec3.cpp
@@ -50,7 +50,7 @@ Vec3 Vec3::operator-(const Vec3& o) const {
 }
 
 Vec3 Vec3::operator*(const Vec3& o) const {
-    return Vec3(x() * o.x(), y() * o.y(), z() - o.z());
+    return Vec3(x() * o.x(), y() * o.y(), z() * o.z());
 }
 
 Vec3 Vec3::operator*(double t) const { return t * (*this); }
