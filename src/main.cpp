c7d5d9b369bba0ddcef61b236b0344dcc6cfdea5
MortalFlame21 <markgio2100@gmail.com>
2026-03-14 00:14:26 +1100

    main.cpp. Update.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@ constexpr auto g_SAMPLES_PER_PIXEL{100};
 constexpr auto g_MAX_DEPTH{50};
 
 auto material_ground{std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0))};
-auto material_left{std::make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.3)};
+auto material_left{std::make_shared<Dielectric>(1.5)};
+auto material_bubble{std::make_shared<Dielectric>(1.0 / 1.5)};
 auto material_center{std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5))};
 auto material_right{std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0)};
 
@@ -21,6 +22,7 @@ int main() {
     HittableList world(
         {std::make_shared<Sphere>(Vec3(0, -100.5, -1), 100, material_ground),
          std::make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.5, material_left),
+         std::make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.4, material_bubble),
          std::make_shared<Sphere>(Vec3(0.0, 0.0, -1.2), 0.5, material_center),
          std::make_shared<Sphere>(Vec3(1.0, 0.0, -1.0), 0.5, material_right)});
 
