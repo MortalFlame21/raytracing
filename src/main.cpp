558610f884ee2560aba2a65b13e13bc3ba37f793
MortalFlame21 <markgio2100@gmail.com>
2026-03-15 18:19:09 +1100

    main.cpp. Update new final scene.

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,64 @@
 #include "Vec3.h"
 
 constexpr auto g_ASPECT_RATIO{16.0 / 9.0};
-constexpr auto g_WIDTH{400};
+static int g_WIDTH{400};
 constexpr auto g_SAMPLES_PER_PIXEL{100};
 constexpr auto g_MAX_DEPTH{50};
 constexpr auto g_VFOV{20};
-const auto g_LOOK_FROM{Vec3(-2, 2, 1)};
-const auto g_LOOK_AT{Vec3(0, 0, -1)};
+const auto g_LOOK_FROM{Vec3(13, 2, 3)};
+const auto g_LOOK_AT{Vec3()};
 const auto g_VEC_UP{Vec3(0, 1, 0)};
-constexpr auto g_DEFOCUS_ANGLE{10.0};
-constexpr auto g_FOCUS_DIST{3.4};
+constexpr auto g_DEFOCUS_ANGLE{0.6};
+constexpr auto g_FOCUS_DIST{10.0};
 
-const auto material_ground{std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0))};
-const auto material_center{std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5))};
-const auto material_left{std::make_shared<Dielectric>(1.50)};
-const auto material_bubble{std::make_shared<Dielectric>(1.00 / 1.50)};
-const auto material_right{std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0)};
+const auto material_ground{std::make_shared<Lambertian>(Color(0.5, 0.5, 0.5))};
+const auto material_1{std::make_shared<Dielectric>(1.5)};
+const auto material_2{std::make_shared<Lambertian>(Color(0.4, 0.2, 0.1))};
+const auto material_3{std::make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0)};
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr
+            << "USAGE: ./rt.out <IMAGE_WIDTH> <NUMBER_SPHERES> > <IMAGE_LOCATION.ppm>\n";
+        return EXIT_FAILURE;
+    }
+
+    g_WIDTH = (std::stoi(argv[1]) > 400) ? std::stoi(argv[1]) : g_WIDTH;
+    int num_spheres{(std::stoi(argv[2]) < 0) ? 2 : 11};
+
     // world
     HittableList world(
-        {std::make_shared<Sphere>(Vec3(0, -100.5, -1), 100, material_ground),
-         std::make_shared<Sphere>(Vec3(0.0, 0.0, -1.2), 0.5, material_center),
-         std::make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.5, material_left),
-         std::make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.4, material_bubble),
-         std::make_shared<Sphere>(Vec3(1.0, 0.0, -1.0), 0.5, material_right)});
+        {std::make_shared<Sphere>(Vec3(0.0, -1000.0, 0.0), 1000, material_ground),
+         std::make_shared<Sphere>(Vec3(0.0, 1.0, 0.0), 1.0, material_1),
+         std::make_shared<Sphere>(Vec3(-4.0, 1.0, 0.0), 1.0, material_2),
+         std::make_shared<Sphere>(Vec3(4.0, 1.0, 0.0), 1.0, material_3)});
+
+    for (int i{-11}; i < num_spheres; ++i) {
+        for (int j{-11}; j < num_spheres; ++j) {
+            auto choose_mat{gen_random_double()};
+            Vec3 center(i + 0.9 * gen_random_double(), 0.2,
+                        j + 0.9 * gen_random_double());
+
+            if (((center - Vec3(4.0, 0.2, 0.0)).length() > 0.9)) {
+                std::shared_ptr<Material> mat{};
+                auto r{gen_random_double(0.2, 0.5)};
+
+                if (choose_mat < 0.8) {
+                    auto albedo = Color::gen_random() * Color::gen_random();
+                    mat = std::make_shared<Lambertian>(albedo);
+                    world.push_back(std::make_shared<Sphere>(center, r, mat));
+                } else if (choose_mat < 0.95) {
+                    auto albedo{Color::gen_random(0.5, 1.0)};
+                    auto fuzz{gen_random_double(0, 0.5)};
+                    mat = std::make_shared<Metal>(albedo, fuzz);
+                    world.push_back(std::make_shared<Sphere>(center, r, mat));
+                } else {
+                    mat = std::make_shared<Dielectric>(1.5);
+                    world.push_back(std::make_shared<Sphere>(center, r, mat));
+                }
+            }
+        }
+    }
 
     // camera
     Camera camera(g_ASPECT_RATIO, g_WIDTH, g_SAMPLES_PER_PIXEL, g_MAX_DEPTH, g_VFOV,
